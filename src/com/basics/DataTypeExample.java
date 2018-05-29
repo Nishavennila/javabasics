0257cf666ad4ff25c4f1f212aa2f35643b7c8d83
Nishavennila <nishanaveen107@gmail.com>
2018-05-29 17:34:34 +0530

    string Example

--- src/com/basics/DataTypeExample.java
+++ src/com/basics/DataTypeExample.java
@@ -1,5 +1,19 @@
 package com.basics;
 
-public class DataTypeExample {
+public class DataTypeExample
+
+{
+	public static void main(String[] args) {
+		int s = 8;
+		System.out.println(s);
+
+		s--;
+
+		System.out.println(s);
+
+		s++;
+		System.out.println(s);
+
+	}
 
 }
