6b522b2ef59a3abed5637019dd721935b675acb2
Nishavennila <nishanaveen107@gmail.com>
2018-05-30 15:33:41 +0530

    basics example

---
+++ src/com/basics/operator.java
@@ -0,0 +1,10 @@
+package com.basics;
+
+public class operator {
+public static void main(String[] args)
+{ int a=13,b=7;
+	int v=(a<b)?100:200;
+	System.out.println(v);
+		
+}
+}
---
+++ src/com/basics/switchcase.java
@@ -0,0 +1,23 @@
+package com.basics;
+
+public class switchcase {
+	public static void main(String[] args)
+	{ 
+		int a=20;
+switch(a) {
+case 10:
+	System.out.println("nisha");
+	break;
+case 20:
+	System.out.println("elakkiya");
+	break;
+default:
+	
+	System.out.println("none");
+	break;
+	
+
+
+		
+} }
+}
