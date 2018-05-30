b5ce1cff0d2a94cb6940078505344963aca54061
Nishavennila <nishanaveen107@gmail.com>
2018-05-30 10:52:58 +0530

    basics example

---
+++ src/com/basics/total.java
@@ -0,0 +1,14 @@
+package com.basics;
+
+public class total {
+public static void main(String[] args)
+{ int x=1,total=0;
+while (x<=10)
+{
+	total+=x;
+	++x;
+	System.out.println(total);
+	
+}
+}
+}
