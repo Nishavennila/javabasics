e87ebc1f00de22e56334762b2f9550b06087b639
Nishavennila <nishanaveen107@gmail.com>
2018-05-30 17:30:47 +0530

    basics example

---
+++ src/com/basics/mystery.java
@@ -0,0 +1,21 @@
+package com.basics;
+
+public class mystery {
+	public static void main(String[] args)
+	{
+		int x=0,y,total=0;
+		while(x<=10)
+		{
+			y=x*x;
+			System.out.println(y);
+			total+=y;
+			++x;
+		}
+		System.out.printf("total is %d\n",total);
+		}
+
+		
+		
+	}
+
+
