7b92eecfabbd367c835b80b7368155d65634ec22
Nishavennila <nishanaveen107@gmail.com>
2018-05-30 16:35:06 +0530

    basics example

---
+++ src/com/basics/while1.java
@@ -0,0 +1,17 @@
+package com.basics;
+
+public class while1 {
+	public static void main(String[] args)
+	{
+		 int y=10,s;
+	
+ while (y> 0 )
+	{
+	 s=y;
+	System.out.println(s);
+	--y;
+	    
+
+	}
+	}
+}
