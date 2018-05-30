30db0a3e4221005d42e8212821eb70efc305be65
Nishavennila <nishanaveen107@gmail.com>
2018-05-30 11:12:41 +0530

    basics example

---
+++ src/com/basics/ifelse.java
@@ -0,0 +1,16 @@
+package com.basics;
+
+public class ifelse {
+public static void main(String[] args)
+{
+int n=2;
+if(n<=10) {
+	System.out.println("greater than or equal to 10");
+}
+	else {
+	System.out.println("less than 10");
+	}
+}
+}
+	
+
