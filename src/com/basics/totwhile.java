01cc3d8725e82d5b25019f9c43108a0f4035cef8
Nishavennila <nishanaveen107@gmail.com>
2018-05-30 16:12:40 +0530

    basics example

---
+++ src/com/basics/totwhile.java
@@ -0,0 +1,16 @@
+package com.basics;
+
+public class totwhile {
+
+public static void main(String[] args){
+
+int x=87,total=0;
+while ( x <= 100 ) {
+total += x;
+++x;
+}
+System.out.println("total is"+total);
+
+
+}	
+}
